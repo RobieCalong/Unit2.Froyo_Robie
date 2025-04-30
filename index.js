0c8e166fbb3f3c4d57aa1e1f5097c0e14b1738d8
robC_vW <robiecalong2@gmail.com>
2025-04-30 13:28:19 -0500

    user input prompt

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // prompt user for flavors
 
+// FOR TESTing purpose
+userInputStr = "vanilla,vanilla,vanilla,strawberry,coffee,coffee";
+
+// UNCOMMENT if you want to prompt the user for an input 
+// userInputStr = prompt("Please enter flavors seperated by ',' a comma");
+
+// checks user input
+// console.log("User Input: ", userInputStr);
+
