1ae381d3ee531524c4d63e6cdcc1dc0acba1ed07
robC_vW <robiecalong2@gmail.com>
2025-04-30 16:16:20 -0500

    updated flavor count order

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 // prompt user for flavors
 
 // FOR TESTing purpose
-userInputStr = "vanilla,vanilla,vanilla,strawberry,coffee,coffee";
+//userFlavor = "vanilla,vanilla,vanilla,strawberry,coffee,coffee";
+// userFlavor = "vanilla,vanilla,vanilla,strawberry,coffee,coffee,blueberry";
 
 // UNCOMMENT if you want to prompt the user for an input 
-// userInputStr = prompt("Please enter flavors seperated by ',' a comma");
+userFlavor = prompt("Please enter flavors seperated by ',' a comma");
 
-// checks user input
-// console.log("User Input: ", userInputStr);
+//splits input into an array of strings
+userFlavorOrder = userFlavor.split(",");
 
+//find unique flavors by using Set
+const uniqueFlavors = new Set(userFlavorOrder)
+//convert Set into Array
+const arrayUniqueFlavors = Array.from(uniqueFlavors)
+
+// create an empty object
+let flavorOrders = {};
+
+//initialize object to 0
+arrayUniqueFlavors.forEach(
+	(flavor) => { 
+      flavorOrders[flavor] = 0
+   }
+)
+
+// checks if object is initialized to 0
+console.table(flavorOrders);
+// console.log("___________________________");
+
+function count(array) {
+	for(let i = 0; i < array.length; i++){
+		key = array[i]
+		flavorOrders[key]++
+	}
+  return flavorOrders;
+}
+
+console.table(count(userFlavorOrder));
